5fff0860f181d7424b41272df0f9040e0af4b377
salmanmarwari1988 <salmanmarwari1988@gmail.com>
2025-12-21 04:53:28 +0500

    Fix build errors and update project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,7 +64,7 @@ export default function RootLayout({
             `,
           }}
         />
-        <!-- End Google Analytics Verification -->
+        {/* End Google Analytics Verification */}
         {/* Google AdSense */}
         <script 
           async 
