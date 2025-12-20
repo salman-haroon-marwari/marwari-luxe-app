a71707bbe3396d19da5534a34531a6440e214f0e
salmanmarwari1988 <salmanmarwari1988@gmail.com>
2025-12-21 04:49:14 +0500

    Fix build errors and update project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,10 +45,26 @@ export default function RootLayout({
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+              })(window,document,'script','dataLayer','GTM-TWM9JZ6R');
             `,
           }}
         />
+        {/* Google Analytics Verification for Search Console */}
+        <script 
+          async 
+          src={`https://www.googletagmanager.com/gtag/js?id=G-HZ9NPP86JZ`}>
+        </script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-HZ9NPP86JZ');
+            `,
+          }}
+        />
+        <!-- End Google Analytics Verification -->
         {/* Google AdSense */}
         <script 
           async 
@@ -68,7 +84,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe 
-            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TWM9JZ6R"
             height="0" 
             width="0" 
             style={{ display: 'none', visibility: 'hidden' }}
