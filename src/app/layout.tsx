fa27ca543df6fd6967f3510d66cecf0a1c08735f
salmanmarwari1988 <salmanmarwari1988@gmail.com>
2025-12-21 05:20:47 +0500

    Fix build errors and update project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,19 +37,14 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="google-site-verification" content="dzryDgptB67-84_p9NLWotI6TuhQYJC-xqTiEZsrPX0" />
-        {/* Google Tag Manager */}
-        <script 
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-TWM9JZ6R');
-            `,
-          }}
-        />
-        {/* Google Analytics Verification for Search Console */}
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TWM9JZ6R');</script>
+        <!-- End Google Tag Manager -->
+        <!-- Google Analytics Verification for Search Console -->
         <script 
           async 
           src={`https://www.googletagmanager.com/gtag/js?id=G-HZ9NPP86JZ`}>
@@ -64,7 +59,7 @@ export default function RootLayout({
             `,
           }}
         />
-        {/* End Google Analytics Verification */}
+        <!-- End Google Analytics Verification -->
         {/* Google AdSense */}
         <script 
           async 
