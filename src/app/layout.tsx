fea626ff37447fbc667b74dcbd050984bcca87ee
salman-haroon-marwari <luxemarwari@gmail.com>
2026-04-10 03:10:50 +0500

    Update Google Tag Manager ID from GTM-TWM9JZ6R to GTM-W6V6QP62

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TWM9JZ6R');
+            })(window,document,'script','dataLayer','GTM-W6V6QP62');
           `,
         }} />
         {/* End Google Tag Manager */}
@@ -139,12 +139,13 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TWM9JZ6R"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W6V6QP62"
             height="0" 
             width="0" 
             style={{ display: 'none', visibility: 'hidden' }}
           />
         </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
         <SpeedInsights />
       </body>
